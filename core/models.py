cced139dbf57156b7684d8a11a11479949b2708a
kauane <kauane@maisresultado.com>
2021-04-29 20:23:55 -0300

    ajuste

--- core/models.py
+++ core/models.py
@@ -18,7 +18,7 @@ class Base (models.Model):
 class Produto(Base):
     nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
-    estoque = models.IntegerField('Estoque', max_length=100)
+    estoque = models.IntegerField('Estoque')
     imagem = StdImageField('Imagem', upload_to='produtos', variations={'thumb': (124, 124)})
     slug = models.CharField('Slug', max_length=100, blank=True, editable=False)
 
